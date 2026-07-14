51ff488471e4b68e86a8d63cc532e3e8bd81f0e1
Cataldo03 <cataldofortino100@gmail.com>
2026-07-14 16:24:35 +0200

    Add strutture.h with data structures and functions

---
+++ strutture.h
@@ -0,0 +1,54 @@
+#ifndef STRUTTURE_H_INCLUDED
+#define STRUTTURE_H_INCLUDED
+
+#include <stdbool.h>
+
+
+typedef struct {
+    int giorno;
+    int mese;
+    int anno;
+} Data;
+
+
+typedef struct spedizione {
+    Data dataOperazione;
+    char tracking[15];          
+    char tipoSpedizione[30];
+    char destinatario[50];
+    double costoSpedizione;
+    struct spedizione *next;
+} Spedizione;
+
+
+typedef struct utente {
+    char username[50];
+    char password[50];
+    char codiceCliente[15];
+    double saldoCredito;
+    Spedizione *storico;
+    struct utente *next;
+} Utente;
+
+typedef Utente* ListaUtenti;
+
+
+typedef struct nodoCoda {
+    char usernameMittente[50];
+    char codiceDestinatario[15];
+    char tracking[15];           
+    double costoSpedizione;
+    struct nodoCoda *next;
+} NodoCodaSpedizione;
+
+typedef struct {
+    NodoCodaSpedizione *front;
+    NodoCodaSpedizione *rear;
+} CodaSpedizioni;
+
+
+Utente* creaUtente(const char* username, const char* password, const char* codiceCliente);
+void inserisciUtente(ListaUtenti* lista, Utente* nuovoUtente);
+Utente* cercaUtentePerUsername(ListaUtenti lista, const char* username);
+Utente* cercaUtentePerCodice(ListaUtenti lista, const char* codiceCliente);
+bool eliminaUtente(ListaUtenti* lista, const char* username);
