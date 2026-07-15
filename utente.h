37dae20850896a2bfee754b81bb6dde75806930a
Cataldo03 <cataldofortino100@gmail.com>
2026-07-15 11:50:19 +0200

    Add header file for user management functions

---
+++ utente.h
@@ -0,0 +1,8 @@
+#ifndef UTENTE_H_INCLUDED
+#define UTENTE_H_INCLUDED
+#include "strutture.h"
+
+void salvaSuFile(ListaUtenti lista, const char* nomeFile);
+ListaUtenti caricaDaFile(const char* nomeFile);
+
+#endif 
