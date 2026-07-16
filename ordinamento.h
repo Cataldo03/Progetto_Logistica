f9d302abb09f0262ac1e8bad638bf9abc2596f5a
Cataldo03 <cataldofortino100@gmail.com>
2026-07-16 23:41:16 +0200

    Add ordinamento.h header file with function declarations

---
+++ ordinamento.h
@@ -0,0 +1,10 @@
+#ifndef ORDINAMENTO_H_INCLUDED
+#define ORDINAMENTO_H_INCLUDED
+
+#include "strutture.h"
+
+void ordinaStoricoPerCosto(Utente* u);
+void stampaStorico(Utente* u);
+void cercaPerTracking(Utente* u, const char* trackDaCercare);
+
+#endif
