1ad291410b3d2e9b1f3efc9c350abbc6fcfb576e
Cataldo03 <cataldofortino100@gmail.com>
2026-07-18 11:37:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "strutture.h"
+#include "implementazione.h"
+#include "ordinamento.h"
+#include "utente.h"
 
+#define NOME_FILE "dati_logistica.txt"
+
+void pulisciBuffer() {
+    while(getchar() != '\n');
+}
+
+int chiediScelta() {
+    int s;
+    printf("\nScelta: ");
+    scanf("%d", &s);
+    pulisciBuffer();
+    return s;
+}
+
+int main() {
+    ListaUtenti db = caricaDaFile(NOME_FILE);
+    CodaSpedizioni coda;
+    inizializzaCoda(&coda);
